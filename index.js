dfcfcce6c92da943ba102367419d2a113062e06b
wangziwen-hubei <1556319679@qq.com>
2020-05-25 23:02:38 +0800

    [Type]: 需求 [Description]: webpack简单配置案例 [Author]: 子文 [Review]: 无

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import './css/index.less';
 
 const div = document.createElement('div');
-div.innerText = '儿子';
+div.innerText = '举个栗子';
 document.getElementById('app').appendChild(div);
 console.log(66666666666);
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ webpack会require获取这个模块的配置然后执行相应的操作*/
 module.exports = {   
     /*development 开发环境 启动devserver后浏览器souces可断点js
     production生产环境 浏览器souces打开是编译后文件（压缩过）。*/
-    mode: 'development',   
+    // mode: 'development',   // 通过命令传递--mode 配置参数
     entry: './index.js',   //打包入口文件
     output: {
         filename: '[name].js',   //输出文件名
