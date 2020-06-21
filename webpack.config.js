d7292b4bbefa34fdac9537f2f898b2b7e4b54986
wangziwen-hubei <1556319679@qq.com>
2020-06-21 13:46:20 +0800

    [Type]: 需求 [Description]: webpack简单配置案例-->设置host [Author]: 子文 [Review]: 无

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /*module是nodejs的模块系统 意思是输出一个模块 
 使用npm run dev时(package.json中配置的脚本)
 webpack会require获取这个模块的配置然后执行相应的操作*/
 
-module.exports = {   
+module.exports = {
     /*development 开发环境 启动devserver后浏览器souces可断点js
     production生产环境 浏览器souces打开是编译后文件（压缩过）。*/
     // mode: 'development',   // 通过命令传递--mode 配置参数
@@ -35,6 +35,7 @@ module.exports = {
         port: 3000, //设置端口号
         hot: true, //热更新
         contentBase: './', //path.join(__dirname, 'dist'), //默认打开文件位置
-        open: false  //自动打开
+        open: false,  //自动打开
+        host: '0.0.0.0', // 默认是 localhost。如果你希望服务器外部可访问则设置为'0.0.0.0'或者设置为本机ip
     }
 };
